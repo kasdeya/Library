fd5ea12ada1fff860739a40e22de6242d359f15e
kas <kasdeyaa@hotmail.com>
2022-09-19 09:02:42 -0700

    seems to be fully working. needs styling

--- script.js
+++ script.js
@@ -31,7 +31,6 @@ function Book(title, author, pages, read, readStatus) {
 }
 }
 
-
 submitButton.addEventListener('click', () => {
  bookTitle = document.getElementById('title').value;
  bookAuthor = document.getElementById('author').value;
@@ -52,20 +51,32 @@ submitButton.addEventListener('click', () => {
     let cellAuthor_i = document.createElement("h3");
     let cellPages_i = document.createElement("h3");
     let cellStatus_i = document.createElement("h3");
+    let removeButton_i = document.createElement("button");
+    removeButton_i.innerText = 'Remove'
     libraryContainer.appendChild(cell_i).className = "library-item";
     cell_i.appendChild(cellTitle_i).classname = "item-title";
     cell_i.appendChild(cellAuthor_i).classname = "item-author";
     cell_i.appendChild(cellPages_i).classname = "item-pages";
     cell_i.appendChild(cellStatus_i).classname = "item-status";
+    cell_i.appendChild(removeButton_i).classname = "remove-button";
     cellTitle_i.innerText = `${myLibrary[i].title}`
     cellAuthor_i.innerText = `${myLibrary[i].author}`
     cellPages_i.innerText = `${myLibrary[i].pages}`
     cellStatus_i.innerText = `${myLibrary[i].readStatus}`
     /* cell.innerText = `${myLibrary[i].info()}`; */
+    removeButton_i.addEventListener('click', () => {
+      myLibrary = myLibrary.filter(data => data.title != cellTitle_i.innerText)
+      cell_i.innerText = ''
+    });
   }
-
-    
 });
+
+  // function removeBookFromLibrary(title, author, pages, status) {
+  //   title.innerText = ''
+  //   author.innerText = ''
+  //   pages.innerText = ''
+  //   status.innerText = ''
+  // }
 
 function addBookToLibrary() {
    /* book1 = new Book(bookTitle, bookAuthor, bookPages, false); */
